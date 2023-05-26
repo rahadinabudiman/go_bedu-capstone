d0410c85dcb55550719406de6a9c8622001e5de1
r4ha <erbees.50000@gmail.com>
2023-05-26 23:12:57 +0700

    Update: Get ID from JWT Cookie

--- controllers/article.go
+++ controllers/article.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"go_bedu/dtos"
 	"go_bedu/helpers"
+	m "go_bedu/middlewares"
 	"go_bedu/usecase"
 	"net/http"
 	"strconv"
@@ -61,6 +63,181 @@ func (c *articleController) GetAllArticles(ctx echo.Context) error {
 			page,
 			limit,
 			count,
+		),
+	)
+}
+
+// Controller for get Article by ID from parameter
+func (c *articleController) GetArticleById(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to get article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	article, err := c.articleUsecase.GetArticleByID(uint(id))
+	if err != nil {
+		return ctx.JSON(
+			http.StatusInternalServerError,
+			helpers.NewErrorResponse(
+				http.StatusInternalServerError,
+				"Failed to get article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	return ctx.JSON(
+		http.StatusOK,
+		helpers.NewResponse(
+			http.StatusOK,
+			"Successfully get article",
+			article,
+		),
+	)
+}
+
+// Controller for Create Article and save to DB
+func (c *articleController) CreateArticle(ctx echo.Context) error {
+	var articleInput dtos.CreateArticlesRequest
+	// Get Admin id from JWT Cookie
+	id, _ := m.IsAdmin(ctx)
+	articleInput.AdministratorID = uint(id)
+
+	if err := ctx.Bind(&articleInput); err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to bind article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	article, err := c.articleUsecase.CreateArticle(&articleInput)
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to create article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	return ctx.JSON(
+		http.StatusCreated,
+		helpers.NewResponse(
+			http.StatusCreated,
+			"Successfully create article",
+			article,
+		),
+	)
+}
+
+// Controller for update Article by ID from Param
+func (c *articleController) UpdateArticle(ctx echo.Context) error {
+	var articleInput dtos.UpdateArticlesRequest
+	// Get Admin id from JWT Cookie
+	id, _ := m.IsAdmin(ctx)
+	articleInput.AdministratorID = uint(id)
+
+	if err := ctx.Bind(&articleInput); err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to bind article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to get article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	article, err := c.articleUsecase.GetArticleByID(uint(id))
+	if article.ArticleID == 0 {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to get article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	articleRespon, err := c.articleUsecase.UpdateArticle(uint(id), articleInput)
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to update article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	return ctx.JSON(
+		http.StatusOK,
+		helpers.NewResponse(
+			http.StatusOK,
+			"Article updated successfully",
+			articleRespon,
+		),
+	)
+}
+
+// Controller for delete article by id from params
+func (c *articleController) DeleteArticle(ctx echo.Context) error {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to get article ID",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	err = c.articleUsecase.DeleteArticle(uint(id))
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			helpers.NewErrorResponse(
+				http.StatusBadRequest,
+				"Failed to delete article",
+				helpers.GetErrorData(err),
+			),
+		)
+	}
+
+	return ctx.JSON(
+		http.StatusOK,
+		helpers.NewResponseMessage(
+			http.StatusOK,
+			"Article deleted successfully",
 		),
 	)
 }
