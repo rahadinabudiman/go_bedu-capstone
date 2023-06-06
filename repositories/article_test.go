fb7b7301975977ff184eb1d6f61083fb832dba1b
r4ha <erbees.50000@gmail.com>
2023-06-06 13:25:33 +0700

    Update: Unit Testing Repo & Change .env.example

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -37,5 +37,53 @@ func TestArticleRepository(t *testing.T) {
 		CreatedArticle, err := articleRepository.CreateArticle(article)
 		assert.NoError(t, err)
 		assert.NotZero(t, CreatedArticle.ID)
+
+		// Check if the article exist already
+		dbArticle, err := articleRepository.GetArticleByID(uint(CreatedArticle.ID))
+		assert.NoError(t, err)
+		assert.Equal(t, CreatedArticle.ID, dbArticle.ID)
+	})
+
+	t.Run("Update Article", func(t *testing.T) {
+		// Get Last Article ID
+		lastArticle := models.Article{}
+		result := db.Last(&lastArticle)
+		assert.NoError(t, result.Error)
+
+		lastArticle.Thumbnail = "thumbnailupdate.jpg"
+		lastArticle.Title = "Test Update Article"
+		lastArticle.Abstract = "This is an updated article"
+		lastArticle.Description = "Updated Lorem ipsum dolor sit amet"
+		lastArticle.Image = "imageupdate.jpg"
+		lastArticle.Label = "Test Update"
+		lastArticle.Slug = "test-update"
+
+		updated, err := articleRepository.UpdateArticle(lastArticle)
+		assert.NoError(t, err)
+
+		// Verify Updated Article
+		assert.Equal(t, lastArticle.Slug, updated.Slug)
+	})
+
+	t.Run("Test Delete Article", func(t *testing.T) {
+		// Get Last Article ID
+		lastArticle := models.Article{}
+		result := db.Last(&lastArticle)
+		assert.NoError(t, result.Error)
+
+		// Get Article By ID
+		article, err := articleRepository.GetArticleByID(uint(lastArticle.ID))
+		assert.NoError(t, err)
+
+		// Delete Article
+		err = articleRepository.DeleteArticle(article)
+		assert.NoError(t, err)
+
+		// Verify Article was Deleted
+		DeletedArticle, err := articleRepository.GetArticleByID(uint(article.ID))
+
+		// Check if the deleted article is empty
+		assert.Error(t, err)
+		assert.Equal(t, models.Article{}, DeletedArticle)
 	})
 }
